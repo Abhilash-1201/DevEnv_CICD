bf63e2ba7ef84dd854bb9435a89345bfcc4a0578
Abhilash-1201 <83076098+Abhilash-1201@users.noreply.github.com>
2023-02-02 07:41:33 +0530

    Updated iam service

---
+++ iam.tf
@@ -0,0 +1,59 @@
+resource "aws_iam_role" "test_role" {
+  name = "EC2_S3_FULL_ACCESS_SONAR"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+}
+resource "aws_iam_policy" "test_policy" {
+  name = "test_sonar"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:*"
+            ],
+            "Resource": "*"
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ecr:*"
+            ],
+            "Resource": "*"
+        }
+    ]
+  })
+}
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.test_role.name
+  policy_arn = aws_iam_policy.test_policy.arn
+}
+resource "aws_iam_instance_profile" "test_profile" {
+  name = "EC2_S3_FULL_ACCESS_SONAR"
+  role = aws_iam_role.test_role.name
+}
